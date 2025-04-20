e9fc9b13e2d1b0a3081d978959b1af826c82a24c
Arthur Pruvost Rivière <96424611+Arthur-91140@users.noreply.github.com>
2025-04-21 00:06:29 +0200

    Update chase.js

--- chase.js
+++ chase.js
@@ -10,23 +10,32 @@ element.style.transform = `translate(${posX}px, ${posY}px)`;
 document.addEventListener('mousemove', function(event) {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
-
+    
     const rect = element.getBoundingClientRect();
     const centerX = rect.left + rect.width / 2;
     const centerY = rect.top + rect.height / 2;
-
+    
     let deltaX = centerX - mouseX;
     let deltaY = centerY - mouseY;
-
+    
     const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
-    if (distance < THRESHOLD_DISTANCE) { 
+    if (mouseX >= rect.left && mouseX <= rect.right && 
+        mouseY >= rect.top && mouseY <= rect.bottom) {
+
+        posX = Math.random() * (window.innerWidth - rect.width);
+        posY = Math.random() * (window.innerHeight - rect.height);
+        
+        element.style.transform = `translate(${posX}px, ${posY}px)`;
+    } 
+
+    else if (distance < THRESHOLD_DISTANCE) { 
         const speed = SPEED / distance;
         posX += deltaX * speed;
         posY += deltaY * speed;
-
+        
         posX = Math.max(0, Math.min(window.innerWidth - rect.width, posX));
         posY = Math.max(0, Math.min(window.innerHeight - rect.height, posY));
-
+        
         element.style.transform = `translate(${posX}px, ${posY}px)`;
     }
 });
